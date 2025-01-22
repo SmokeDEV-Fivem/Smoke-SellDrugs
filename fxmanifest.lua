56e0147e6db8a52ca74f6f6e095d76957c0763d4
SmokeDEV <smokeshoppsd@gmail.com>
2025-01-22 18:56:51 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 
 author 'SmokeDEV'
-description 'Script de vente de drogue aux PNJ de SmokeShop
+description 'Script de vente de drogue aux PNJ de SmokeShop'
 version '1.0.0'
 
 shared_scripts {
