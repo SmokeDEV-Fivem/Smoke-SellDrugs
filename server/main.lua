d38612f53d2487d3795c041017cc4584dd1643a8
SmokeDEV <smokeshoppsd@gmail.com>
2025-01-23 03:24:49 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,11 +1,43 @@
 local ESX = exports["es_extended"]:getSharedObject()
 
+local function GetCopsCount()
+    local cops = 0
+    local xPlayers = ESX.GetPlayers()
+    
+    for i=1, #xPlayers do
+        local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+        if xPlayer.job.name == 'police' then
+            cops = cops + 1
+        end
+    end
+    return cops
+end
+
+RegisterNetEvent('smoke-selldrugs:checkCops')
+AddEventHandler('smoke-selldrugs:checkCops', function(ped, pedNetId)
+    local source = source
+    local xPlayer = ESX.GetPlayerFromId(source)
+    
+    if not xPlayer then return end
+    
+    if GetCopsCount() >= Config.MinimumCops then
+        TriggerClientEvent('smoke-selldrugs:continueVente', source, pedNetId)
+    else
+        xPlayer.showNotification("~r~Il n'y a pas assez de policiers en ville pour vendre de la drogue")
+    end
+end)
+
 RegisterNetEvent('smoke-selldrugs:sellDrug')
 AddEventHandler('smoke-selldrugs:sellDrug', function(drugName, quantity, price)
     local source = source
     local xPlayer = ESX.GetPlayerFromId(source)
     
     if not xPlayer then return end
+    
+    if GetCopsCount() < Config.MinimumCops then
+        xPlayer.showNotification("~r~Il n'y a pas assez de policiers en ville pour vendre de la drogue")
+        return
+    end
     
     local count = exports.ox_inventory:GetItemCount(source, drugName)
     
